3a6e4f7dfea7b2b4d4d9b09155899ad8b9a8bfca
Mariana <Mariana@10.10.26.38>
2014-07-25 17:04:42 -0300

    Inserção javaaplication9

---
+++ JavaApplication9/src/javaapplication9/JavaApplication9.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication9;
+
+/**
+ *
+ * @author Mariana
+ */
+public class JavaApplication9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Teste para disciplina de engenharia de software\n");
+        System.out.println("Escrevendo ças coisas ae\n");
+        System.out.println("Pois é\n");
+    }
+    
+}
