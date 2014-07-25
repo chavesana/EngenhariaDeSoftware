b024a2828a9f0e16b3c56a7da29a7fc33bf96713
Gabs <Gabs@dell-PC>
2014-07-25 17:03:49 -0300

    Inclusão de um método.

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -12,25 +12,31 @@ public class JavaApplication1 {
 
     public static void main(String[] args) {
         JavaApplication1 r = new JavaApplication1();
-        System.out.println("Resposta do item a) do 4): " + r.x(4, 2));
+        System.out.println("Resposta do item a) do 4): " + r.x(4, 1));
         System.out.println("Resposta do item a) do 5): " + r.y(6));
         System.out.println("blablabla");
     }
  
-	int y(int n){
+    int y(int n){
         if(n >= 0 && n <=2)
-                return n;
+            return n;
         else 
-                return y(n-1) + y(n-2) + y(n-3);
+            return y(n-1) + y(n-2) + y(n-3);
     }
         
         
-        int x(int n, int m){
-                if (n == m || n == 0)
-                    return 1;
-                else
-                    return x(n-1, m) + x(n-1, m+1);
-            } 
-
+    int x(int n, int m){
+        if (n == m || n == 0)
+            return 1;
+        else
+            return x(n-1, m) + x(n-1, m+1);
+    }
+    
+    int potencia(int n, int m){
+        int value = n;
+        for (int x = 1; x <m; x++)
+            value = value *n;
+        return value;
+    }
 
 }
